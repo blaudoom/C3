4a85cb7bc53b6b225b599296aec0dc840ea0eb96
blaudoom <1763799+blaudoom@users.noreply.github.com>
2021-08-12 14:29:29 +0300

    migrate from old repo

---
+++ shells.py
@@ -0,0 +1,66 @@
+shells = [{
+    'name': 'bash',
+    'address': True,
+    'port': True,
+    'file': 'bash.sh'
+},
+ {
+     'name': 'perl',
+     'address': True,
+     'port': True,
+     'file': 'perl.pl'
+ },
+ {
+     'name': 'python2',
+     'address': True,
+     'port': True,
+     'file': 'python2.py'
+ },
+ {
+     'name': 'php',
+     'address': True,
+     'port': True,
+     'file': 'php.php'
+ },
+ {
+     'name': 'ruby',
+     'address': True,
+     'port': True,
+     'file': 'ruby.rb'
+ },
+ {
+     'name': 'netcat_c',
+     'address': True,
+     'port': True,
+     'file': 'netcat_c.sh'
+ },
+ {
+     'name': 'netcat_e',
+     'address': True,
+     'port': True,
+     'file': 'netcat_e.sh'
+ },
+ {
+     'name': 'netcat_fifo',
+     'address': True,
+     'port': True,
+     'file': 'netcat_fifo.sh'
+ },
+ {
+     'name': 'netcat_node',
+     'address': True,
+     'port': True,
+     'file': 'netcat_node.sh'
+ },
+ {
+     'name': 'telnet',
+     'address': True,
+     'port': True,
+     'file': 'telnet.sh'
+ },
+ {
+     'name': 'netcat_cmd',
+     'address': True,
+     'port': True,
+     'file': 'netcat_cmd.bat'
+ }]
